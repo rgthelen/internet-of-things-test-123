d4913901338c0a5d9a56fd9fd26256c67ce013c9
Rob Thelen <rgthelen@gmail.com>
2018-08-24 12:31:53 -0400

    Update bluemix-settings.js
    change "/red"

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -40,7 +40,7 @@ var settings = module.exports = {
     autoInstallModules: true,
 
     // Move the admin UI
-   // httpAdminRoot: '/red',
+    httpAdminRoot: '/',
 
     // You can protect the user interface with a userid and password by using the following property
     // the password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
